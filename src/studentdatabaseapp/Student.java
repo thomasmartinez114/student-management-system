f5cce17728ccfc3a6137d2d518c889c247acd80b
Thomas Martinez <tmartinez114@gmail.com>
2020-11-23 21:35:50 -0500

    added comments for methods

--- src/studentdatabaseapp/Student.java
+++ src/studentdatabaseapp/Student.java
@@ -1,4 +1,16 @@
 package studentdatabaseapp;
 
 public class Student {
+
+    // Constructor: prompts user to enter student's name and year
+
+    // Generate an 5 digit ID
+
+    // Enroll in courses
+
+    // View balance
+
+    // Pay tuition
+
+    // Show status
 }
--- src/studentdatabaseapp/StudentDatabaseApp.java
+++ src/studentdatabaseapp/StudentDatabaseApp.java
@@ -3,6 +3,8 @@ package studentdatabaseapp;
 public class StudentDatabaseApp {
 
     public static void main(String[] args) {
+        // Ask how many new users we want to add
 
+        //  Create n number of students
     }
 }

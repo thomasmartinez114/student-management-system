732d05bb0863b948f06dc213ba796af77f1b7cb1
Thomas Martinez <tmartinez114@gmail.com>
2020-11-24 22:46:23 -0500

    variables set

--- src/studentdatabaseapp/Student.java
+++ src/studentdatabaseapp/Student.java
@@ -2,6 +2,14 @@ package studentdatabaseapp;
 
 public class Student {
 
+    private String firstName;
+    private String lastName;
+    private String gradeYear;
+    private int studentID;
+    private String courses;
+    private int tuitionBalance;
+    private int costOfCourse = 600;
+
     // Constructor: prompts user to enter student's name and year
 
     // Generate an 5 digit ID

08028ac48e67cb77807d9cd73cf7727fc6eb727f
Thomas Martinez <tmartinez114@gmail.com>
2020-11-27 19:49:51 -0500

    Constrcutor

--- src/studentdatabaseapp/Student.java
+++ src/studentdatabaseapp/Student.java
@@ -6,23 +6,25 @@ public class Student {
 
     private String firstName;
     private String lastName;
-    private String gradeYear;
+    private int gradeYear;
     private int studentID;
     private String courses;
     private int tuitionBalance;
-    private int costOfCourse = 600;
+    private static int costOfCourse = 600;
+    private static int id = 1001;
 
     // Constructor: prompts user to enter student's name and year
     public Student() {
         Scanner in = new Scanner(System.in);
-        System.out.print("Enter Student's First Name:" );
+        System.out.print("Enter Student's First Name: " );
         this.firstName = in.nextLine();
 
-        System.out.print("Enter Student's Last Name:" );
+        System.out.print("Enter Student's Last Name: " );
         this.lastName = in.nextLine();
 
-        System.out.print("Enter Student's Grade Level:" );
-        this.gradeYear = in.nextLine();
+        System.out.print("1 - Freshmen\n2 - Sophomore\n3 - Junior\n4 - Senior\nEnter Student's Grade Level: " );
+        this.gradeYear = in.nextInt();
+        System.out.println(firstName + " " + lastName + " " + gradeYear);
     }
 
     // Generate an 5 digit ID
--- src/studentdatabaseapp/StudentDatabaseApp.java
+++ src/studentdatabaseapp/StudentDatabaseApp.java
@@ -3,6 +3,7 @@ package studentdatabaseapp;
 public class StudentDatabaseApp {
 
     public static void main(String[] args) {
+        Student stu1 = new Student();
         // Ask how many new users we want to add
 
         //  Create n number of students
